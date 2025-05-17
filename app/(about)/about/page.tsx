5109284d5d8e58c9b35e30378b72c3bf9e71c0d3
Muhammad Farhan <farhan_75230@hotmail.com>
2025-05-17 12:08:21 +0200

    About page created!

---
+++ app/(about)/about/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function page() {
+  return (
+    <div>
+      <p className="text-center">
+        This is a site i cretaed as Next.js tutorial to be used later on.
+      </p>
+    </div>
+  );
+}
